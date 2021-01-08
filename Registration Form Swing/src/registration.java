1c59a011fb7454a6af648e8e7f0bce79305ae4f0
Barbara0Prado <62604479+Barbara0Prado@users.noreply.github.com>
2021-01-08 13:11:51 +0000

    Add files via upload

---
+++ Registration Form Swing/src/registration.java
@@ -0,0 +1,11 @@
+
+public class registration {
+
+	public static void main(String[] args) throws Exception {
+		
+		/* Creating method to invoke the frame */
+		MyFrame f = new MyFrame();
+
+	}
+
+}
